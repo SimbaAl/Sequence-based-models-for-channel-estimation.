2c7ed8fe9b1127fd2cd4b08f780c9e97aebc0148
Simbarashe Aldrin Ngorima <46420216+SimbaAl@users.noreply.github.com>
2023-10-10 10:28:50 +0200

    Add files via upload
    Scripts used to generate data for the sequence-based models for channel estimation

--- IDX_Generation.m
+++ IDX_Generation.m
@@ -1,8 +1,13 @@
-clc;clearvars; close all;
-IDX = 100; % total number of samples
-TR  = 0.80; % training dataset percentage
-TE  = 0.20; % testing dataset percentage
-All_IDX = 1:IDX;
-training_samples = randperm(IDX,TR*IDX).'; % choose randomly 80% of the total indices for training
-testing_samples = setdiff(All_IDX,training_samples).';
-save(['./samples_indices_',num2str(IDX),'.mat'],  'training_samples','testing_samples'); 
+clc;
+clearvars;
+close all;
+
+% Updated values
+TOTAL_SAMPLES = 18000; % total number of samples (16000 training + 2000 testing)
+TR_RATIO = 16000 / TOTAL_SAMPLES; % training dataset percentage
+TE_RATIO = 2000 / TOTAL_SAMPLES;  % testing dataset percentage
+
+All_IDX = 1:TOTAL_SAMPLES;
+training_samples = randperm(TOTAL_SAMPLES, round(TR_RATIO * TOTAL_SAMPLES)).'; % choose randomly 80% of the total indices for training
+testing_samples = setdiff(All_IDX, training_samples).';
+save(['./samples_indices_', num2str(TOTAL_SAMPLES), '.mat'], 'training_samples', 'testing_samples');
